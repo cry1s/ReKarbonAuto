00f402baf3e79a41ad1b5e49e1935f972c3830a3
cry1s <cry1s@users.noreply.github.com>
2019-12-18 00:41:27 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ReKarbonAuto',
+    version='alpha',
+    packages=[''],
+    url='vk.com/karbonar',
+    license='None',
+    author='cry1s',
+    author_email='cry1s@ya.ru',
+    description='Program for automation of work with the interface',
+    install_requires=['pyautogui', 'Pillow']
+)
